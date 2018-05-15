33b035561a5588ecc97755e0c3a197b069aae522
Kin <zhuangxuxin@hotmail.com>
2018-05-15 23:32:23 +0800

    LKActivitiStatics

---
+++ lichkin-spring-boot-activiti-defines/src/main/java/com/lichkin/framework/defines/activiti/statics/LKActivitiStatics.java
@@ -0,0 +1,15 @@
+package com.lichkin.framework.defines.activiti.statics;
+
+/**
+ * 常量定义
+ * @author SuZhou LichKin Information Technology Co., Ltd.
+ */
+public interface LKActivitiStatics {
+
+	/** 流程主键 */
+	public static final String KEY_PROCESS_KEY = "processKey";
+
+	/** 流程名称 */
+	public static final String KEY_PROCESS_NAME = "processName";
+
+}
