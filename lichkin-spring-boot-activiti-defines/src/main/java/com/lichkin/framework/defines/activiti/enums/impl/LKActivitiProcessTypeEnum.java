08a32947b2062234d3e248159efa029560c9800f
Kin <zhuangxuxin@hotmail.com>
2018-05-16 00:13:14 +0800

    流程类型枚举

---
+++ lichkin-spring-boot-activiti-defines/src/main/java/com/lichkin/framework/defines/activiti/enums/impl/LKActivitiProcessTypeEnum.java
@@ -0,0 +1,14 @@
+package com.lichkin.framework.defines.activiti.enums.impl;
+
+import com.lichkin.framework.defines.enums.LKEnum;
+
+/**
+ * 流程类型枚举
+ * @author SuZhou LichKin Information Technology Co., Ltd.
+ */
+public enum LKActivitiProcessTypeEnum implements LKEnum {
+
+	/** 单线流程 */
+	SINGLE_LINE;
+
+}
