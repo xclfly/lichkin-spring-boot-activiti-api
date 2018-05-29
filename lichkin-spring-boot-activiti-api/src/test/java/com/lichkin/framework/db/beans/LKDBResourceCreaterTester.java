26d55dda3a02d21a1d22482fe2ee829409104c15
Kin <zhuangxuxin@hotmail.com>
2018-05-30 01:08:16 +0800

    数据库资源相关权限修改及代码完善

--- lichkin-spring-boot-activiti-api/src/test/java/com/lichkin/framework/db/beans/LKDBResourceCreaterTester.java
+++ lichkin-spring-boot-activiti-api/src/test/java/com/lichkin/framework/db/beans/LKDBResourceCreaterTester.java
@@ -7,9 +7,9 @@ import org.junit.Test;
 public class LKDBResourceCreaterTester {
 
 	@Test
-	public void test() throws NoSuchMethodException, IOException {
+	public void test() throws IOException {
 		// 在使用框架前需要调用本方法来创建所需的R文件以及RInitializer文件
-		LKDBResourceCreater.createRFiles("/Users/kin/Git/github.com/lichkin-spring-boot-activiti-api/lichkin-spring-boot-activiti-api/src/main/java");
+		LKDBResourceCreater.createRFiles();
 	}
 
 }
